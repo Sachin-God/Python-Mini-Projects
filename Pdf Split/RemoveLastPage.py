26f18568c04e9f669bd44e528e698321f1a9118f
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2024-02-09 23:30:31 +0530

    Delete Pdf Split directory

--- Pdf Split/RemoveLastPage.py
+++
@@ -1,9 +0,0 @@
-import pikepdf
-import os
-
-file = input('Enter your File Name: ')  # file name to be exactly precise and to be in same Directory
-
-with pikepdf.open(file) as pdf:
-    pdfPages = len(pdf.pages)
-    del pdf.pages[-1]
-    pdf.save('output.pdf')
